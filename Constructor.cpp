856750adae05883256e835b36fc6af2c387b24f4
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-14 14:48:12 +0700

    menyelesaikan int main file 1

--- Constructor.cpp
+++ Constructor.cpp
@@ -29,8 +29,18 @@ void mahasiswa::printAll()
 {
     cout << "ID : " << id << endl;
     cout << "Nama : " << nama << endl;
+    cout << endl;
 }
 
 int main(){
+    mahasiswa mhs1("Anomali Dika");
+    mahasiswa mhs2("Panji Dark System");
+    mahasiswa mhs3("Ilam Tidak Ada Indomaret");
+    mahasiswa mhs4("Hafis Samba");
 
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+    return 0;
 }
