2429a8038ac157157122c05f363d7480397cbd59
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-14 15:44:10 +0700

    Create Constructor3.cpp

---
+++ Constructor3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    string nama;
+
+public:
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
+    mahasiswa(int inim)
+    {
+        nim = inim;
+    }
+    mahasiswa(string inama)
+    {
+        nama = inama;
+    };
+    mahasiswa(int inim, string inama)
+    {
+        nim = inim;
+        nama = inama;
+    };
+    void cetak()
+    {
+        cout << "NIM = " << nim << endl;
+        cout << "Nama = " << nama << endl;
+        cout << endl;
+    };
+};
+
+int main()
+{
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("uno 13");
+    mahasiswa mhs4(30, "farhad 15");
+
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
+
+    return 0;
+}
