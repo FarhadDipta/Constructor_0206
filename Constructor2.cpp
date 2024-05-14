43ef248ab2c3e01de4e4d04778e03af7788a4572
FarhadDipta <160616287+FarhadDipta@users.noreply.github.com>
2024-05-14 15:14:11 +0700

    menyelesaikan int main file ke 2

--- Constructor2.cpp
+++ Constructor2.cpp
@@ -30,3 +30,29 @@ void mahasiswa::setID()
 {
     id = ++nim;
 }
+void mahasiswa::printAll()
+{
+    cout << "ID : " << id << endl;
+    cout << "NIM : " << nim << endl;
+    cout << endl;
+}
+
+int main()
+{
+    mahasiswa mhs1("Sri Dadi Purnowo");
+    mahasiswa mhs2("Joko widodo");
+
+    mahasiswa::setNim(206); // mengakses nim melalui static member functiion "setNim"
+    mahasiswa mhs3("Prabowo Subiantoooook");
+    mahasiswa mhs4("Anies Angin KTP");
+
+    mhs1.printAll();
+    mhs2.printAll();
+    mhs3.printAll();
+    mhs4.printAll();
+
+    cout << "akses dari luar object = " << mahasiswa::getNim() << endl; // mengakses nim melalui static member function "getNim"
+
+
+    return 0;
+}
